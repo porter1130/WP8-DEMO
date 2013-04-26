04e629b1ca7cde650707498a1b3af13cde4f72bd
porter wang <wjc1029@live.cn>
2013-04-26 10:23:50 +0800

    add wp8 demo

---
+++ Samples02/TestLifecycle/TestLifecycle/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Phone.Controls;
+using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace TestLifecycle
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            Debug.WriteLine("MainPage ctor");
+
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Debug.WriteLine("OnNavigatedTo");
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Debug.WriteLine("OnNavigatedFrom");
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            Debug.WriteLine("OnNavigatingFrom");
+        }
+    }
+}
---
+++ Samples02/TestObscured/TestObscured/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace TestObscured
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
